732e16dcab225b76b698529e032a0deee1c5f56d
KingLoui <matzehollerbach@web.de>
2016-09-25 17:04:54 +0200

    add cakedc users helper to fix custom views

---
+++ src/View/Helper/AuthLinkHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace KingLoui\BaseKitUsers\View\Helper;
+
+use CakeDC\Users\View\Helper\AuthLinkHelper as BaseHelper;
+
+
+class AuthLinkHelper extends BaseHelper
+{
+
+}
---
+++ src/View/Helper/UserHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace KingLoui\BaseKitUsers\View\Helper;
+
+use CakeDC\Users\View\Helper\UserHelper as BaseHelper;
+
+/**
+ * User helper
+ */
+class UserHelper extends BaseHelper
+{
+
+}

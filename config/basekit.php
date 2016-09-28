f7e7df3599f6f6cfe9df58e0d61803eb1ccf3d58
Matthias Hollerbach <matzehollerbach@web.de>
2016-09-28 10:05:09 +0200

    updated config

--- config/basekit.php
+++ config/basekit.php
@@ -4,18 +4,21 @@ use Cake\Core\Configure;
 use Cake\Routing\Router;
 
 $config = [
-		'BaseKit' => [
-				'AdminMenu' => [
-					'HeaderElement' => 'KingLoui/BaseKitUsers.adminbarheader',
-					'MenuItems' => [
-						'Users' => [
-								['uri' => '#', 'extras' => ['icon' => 'fa fa-user']],
-								'List Users' => ['uri' => ['plugin' => 'KingLoui/BaseKitUsers','controller' => 'Users', 'action' => 'index']],
-								'Add User' => ['uri' => ['plugin' => 'KingLoui/BaseKitUsers','controller' => 'Users', 'action' => 'add']]
-						],
-			    ]
-				]
+	'BaseKit' => [
+		'NavTop' => [
+			'TopLinksElement' => 'KingLoui/BaseKitUsers.toplinks'
+		],
+		'NavSidebar' => [
+			'HeaderElement' => 'KingLoui/BaseKitUsers.adminbarheader',
+			'MenuItems' => [
+				'Users' => [
+						['uri' => '#', 'extras' => ['icon' => 'fa fa-user']],
+						'List Users' => ['uri' => ['plugin' => 'KingLoui/BaseKitUsers','controller' => 'Users', 'action' => 'index']],
+						'Add User' => ['uri' => ['plugin' => 'KingLoui/BaseKitUsers','controller' => 'Users', 'action' => 'add']]
+				],
+	    ]
 		]
+	]
 ];
 
 return $config;
